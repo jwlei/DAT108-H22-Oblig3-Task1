34cb4e8acccb7923cb817f9be2f4a75e6c55d7df
Joachim <54099045+jwlei@users.noreply.github.com>
2022-10-10 23:57:50 +0200

    Adding boilerplate stuff

--- src/main/java/task1/controller/login/LoginController.java
+++ src/main/java/task1/controller/login/LoginController.java
@@ -1,8 +1,14 @@
 package task1.controller.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+
+    @GetMapping(value="/login", produces="text/plain")
+    public String login() {
+        return "login";
+    }
 
 }
